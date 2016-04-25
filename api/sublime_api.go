9f9edaa1d411e0f8e4d04f7a3deb070e1e05ead8
Mohammad Zolfaghari <zidom72@gmail.com>
2016-04-26 01:27:05 +0430

    cleaning api package + better naming

---
+++ api/sublime_api.go
@@ -0,0 +1,114 @@
+// Copyright 2016 The lime Authors.
+// Use of this source code is governed by a 2-clause
+// BSD-style license that can be found in the LICENSE file.
+
+//go:generate go run gen_python_api.go
+
+package api
+
+import (
+	"github.com/limetext/backend"
+	"github.com/limetext/backend/log"
+	"github.com/limetext/backend/render"
+	"github.com/limetext/gopy/lib"
+	"github.com/limetext/util"
+)
+
+var (
+	classes = []struct {
+		name string
+		c    *py.Class
+	}{
+		{"Region", &_regionClass},
+		{"RegionSet", &_region_setClass},
+		{"View", &_viewClass},
+		{"Window", &_windowClass},
+		{"Edit", &_editClass},
+		{"Settings", &_settingsClass},
+		{"WindowCommandGlue", &_windowCommandGlueClass},
+		{"TextCommandGlue", &_textCommandGlueClass},
+		{"ApplicationCommandGlue", &_applicationCommandGlueClass},
+		{"OnQueryContextGlue", &_onQueryContextGlueClass},
+		{"ViewEventGlue", &_viewEventGlueClass},
+	}
+
+	constants = []struct {
+		name     string
+		constant int
+	}{
+		{"OP_EQUAL", int(util.OpEqual)},
+		{"OP_NOT_EQUAL", int(util.OpNotEqual)},
+		{"OP_REGEX_MATCH", int(util.OpRegexMatch)},
+		{"OP_NOT_REGEX_MATCH", int(util.OpNotRegexMatch)},
+		{"OP_REGEX_CONTAINS", int(util.OpRegexContains)},
+		{"OP_NOT_REGEX_CONTAINS", int(util.OpNotRegexContains)},
+		{"INHIBIT_WORD_COMPLETIONS", 0},
+		{"INHIBIT_EXPLICIT_COMPLETIONS", 0},
+		{"LITERAL", int(backend.IGNORECASE)},
+		{"IGNORECASE", int(backend.LITERAL)},
+		{"CLASS_WORD_START", int(backend.CLASS_WORD_START)},
+		{"CLASS_WORD_END", int(backend.CLASS_WORD_END)},
+		{"CLASS_PUNCTUATION_START", int(backend.CLASS_PUNCTUATION_START)},
+		{"CLASS_PUNCTUATION_END", int(backend.CLASS_PUNCTUATION_END)},
+		{"CLASS_SUB_WORD_START", int(backend.CLASS_SUB_WORD_START)},
+		{"CLASS_SUB_WORD_END", int(backend.CLASS_SUB_WORD_END)},
+		{"CLASS_LINE_START", int(backend.CLASS_LINE_START)},
+		{"CLASS_LINE_END", int(backend.CLASS_LINE_END)},
+		{"CLASS_EMPTY_LINE", int(backend.CLASS_EMPTY_LINE)},
+		{"CLASS_MIDDLE_WORD", int(backend.CLASS_MIDDLE_WORD)},
+		{"CLASS_WORD_START_WITH_PUNCTUATION", int(backend.CLASS_WORD_START_WITH_PUNCTUATION)},
+		{"CLASS_WORD_END_WITH_PUNCTUATION", int(backend.CLASS_WORD_END_WITH_PUNCTUATION)},
+		{"CLASS_OPENING_PARENTHESIS", int(backend.CLASS_OPENING_PARENTHESIS)},
+		{"CLASS_CLOSING_PARENTHESIS", int(backend.CLASS_CLOSING_PARENTHESIS)},
+		{"DRAW_EMPTY", int(render.DRAW_EMPTY)},
+		{"HIDE_ON_MINIMAP", int(render.HIDE_ON_MINIMAP)},
+		{"DRAW_EMPTY_AS_OVERWRITE", int(render.DRAW_EMPTY_AS_OVERWRITE)},
+		{"DRAW_NO_FILL", int(render.DRAW_NO_FILL)},
+		{"DRAW_NO_OUTLINE", int(render.DRAW_NO_OUTLINE)},
+		{"DRAW_SOLID_UNDERLINE", int(render.DRAW_SOLID_UNDERLINE)},
+		{"DRAW_STIPPLED_UNDERLINE", int(render.DRAW_STIPPLED_UNDERLINE)},
+		{"DRAW_SQUIGGLY_UNDERLINE", int(render.DRAW_SQUIGGLY_UNDERLINE)},
+		{"PERSISTENT", int(render.PERSISTENT)},
+		{"HIDDEN", int(render.HIDDEN)},
+	}
+)
+
+func init() {
+	l := py.InitAndLock()
+	defer l.Unlock()
+
+	if sys, err := py.Import("sys"); err != nil {
+		log.Warn("Couldn't import sys: %s", err)
+	} else {
+		if pyc, err := py.NewUnicode("dont_write_bytecode"); err != nil {
+			log.Warn(err)
+		} else {
+			// avoid generating pyc files
+			sys.Base().SetAttr(pyc, py.True)
+		}
+		sys.Decref()
+	}
+
+	methods := append(generated_methods, manual_methods...)
+	m, err := py.InitModule("sublime", methods)
+	if err != nil {
+		// TODO: we should handle this as error
+		panic(err)
+	}
+
+	for _, cl := range classes {
+		c, err := cl.c.Create()
+		if err != nil {
+			panic(err)
+		}
+		if err := m.AddObject(cl.name, c); err != nil {
+			panic(err)
+		}
+	}
+
+	for _, c := range constants {
+		if err := m.AddIntConstant(c.name, c.constant); err != nil {
+			panic(err)
+		}
+	}
+}
--- api/sublime_manual.go
+++ api/sublime_manual.go
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a 2-clause
 // BSD-style license that can be found in the LICENSE file.
 
-//go:generate go run gen_python_api.go
-
 package api
 
 import (
@@ -12,10 +10,113 @@ import (
 
 	"github.com/limetext/backend"
 	"github.com/limetext/backend/log"
-	"github.com/limetext/backend/render"
 	"github.com/limetext/gopy/lib"
-	"github.com/limetext/util"
 )
+
+func sublime_ErrorMessage(tu *py.Tuple) (py.Object, error) {
+	var (
+		arg1 string
+	)
+	if v, err := tu.GetItem(0); err != nil {
+		return nil, err
+	} else {
+		if v3, err2 := fromPython(v); err2 != nil {
+			return nil, err2
+		} else {
+			if v2, ok := v3.(string); !ok {
+				return nil, fmt.Errorf("Expected type string for backend.DummyFrontend.ErrorMessage() arg1, not %s", v.Type())
+			} else {
+				arg1 = v2
+			}
+		}
+	}
+	backend.GetEditor().Frontend().ErrorMessage(arg1)
+	return toPython(nil)
+}
+
+func sublime_MessageDialog(tu *py.Tuple) (py.Object, error) {
+	var (
+		arg1 string
+	)
+	if v, err := tu.GetItem(0); err != nil {
+		return nil, err
+	} else {
+		if v3, err2 := fromPython(v); err2 != nil {
+			return nil, err2
+		} else {
+			if v2, ok := v3.(string); !ok {
+				return nil, fmt.Errorf("Expected type string for backend.DummyFrontend.MessageDialog() arg1, not %s", v.Type())
+			} else {
+				arg1 = v2
+			}
+		}
+	}
+	backend.GetEditor().Frontend().MessageDialog(arg1)
+	return toPython(nil)
+}
+
+func sublime_OkCancelDialog(tu *py.Tuple) (py.Object, error) {
+	var (
+		arg1 string
+		arg2 string
+	)
+	if v, err := tu.GetItem(0); err != nil {
+		return nil, err
+	} else {
+		if v3, err2 := fromPython(v); err2 != nil {
+			return nil, err2
+		} else {
+			if v2, ok := v3.(string); !ok {
+				return nil, fmt.Errorf("Expected type string for backend.DummyFrontend.OkCancelDialog() arg1, not %s", v.Type())
+			} else {
+				arg1 = v2
+			}
+		}
+	}
+	if v, err := tu.GetItem(1); err != nil {
+		return nil, err
+	} else {
+		if v3, err2 := fromPython(v); err2 != nil {
+			return nil, err2
+		} else {
+			if v2, ok := v3.(string); !ok {
+				return nil, fmt.Errorf("Expected type string for backend.DummyFrontend.OkCancelDialog() arg2, not %s", v.Type())
+			} else {
+				arg2 = v2
+			}
+		}
+	}
+	ret0 := backend.GetEditor().Frontend().OkCancelDialog(arg1, arg2)
+	var err error
+	var pyret0 py.Object
+
+	pyret0, err = toPython(ret0)
+	if err != nil {
+		return nil, err
+	}
+	return pyret0, err
+}
+
+func sublime_StatusMessage(tu *py.Tuple) (py.Object, error) {
+	var (
+		arg1 string
+	)
+	if v, err := tu.GetItem(0); err != nil {
+		return nil, err
+	} else {
+		if v3, err2 := fromPython(v); err2 != nil {
+			return nil, err2
+		} else {
+			if v2, ok := v3.(string); !ok {
+				return nil, fmt.Errorf("Expected type string for backend.DummyFrontend.StatusMessage() arg1, not %s", v.Type())
+			} else {
+				arg1 = v2
+			}
+		}
+	}
+	backend.GetEditor().Frontend().StatusMessage(arg1)
+	return toPython(nil)
+}
 
 func sublime_Console(tu *py.Tuple, kwargs *py.Dict) (py.Object, error) {
 	if tu.Size() != 1 {
@@ -29,7 +130,7 @@ func sublime_Console(tu *py.Tuple, kwargs *py.Dict) (py.Object, error) {
 	return toPython(nil)
 }
 
-func sublime_set_timeout(tu *py.Tuple, kwargs *py.Dict) (py.Object, error) {
+func sublime_SetTimeOut(tu *py.Tuple, kwargs *py.Dict) (py.Object, error) {
 	var (
 		pyarg py.Object
 	)
@@ -64,103 +165,12 @@ func sublime_set_timeout(tu *py.Tuple, kwargs *py.Dict) (py.Object, error) {
 	return toPython(nil)
 }
 
-var sublime_manual_methods = []py.Method{
+var manual_methods = []py.Method{
 	{Name: "console", Func: sublime_Console},
-	{Name: "set_timeout", Func: sublime_set_timeout},
+	{Name: "set_timeout", Func: sublime_SetTimeOut},
 	{Name: "packages_path", Func: sublime_PackagesPath},
-}
-
-func init() {
-	l := py.InitAndLock()
-	defer l.Unlock()
-
-	if sys, err := py.Import("sys"); err != nil {
-		log.Warn(err)
-	} else {
-		if pyc, err := py.NewUnicode("dont_write_bytecode"); err != nil {
-			log.Warn(err)
-		} else {
-			// avoid pyc files
-			sys.Base().SetAttr(pyc, py.True)
-		}
-		sys.Decref()
-	}
-
-	sublime_methods = append(sublime_methods, sublime_manual_methods...)
-	m, err := py.InitModule("sublime", sublime_methods)
-	if err != nil {
-		// TODO: we should handle this as error
-		panic(err)
-	}
-
-	classes := []struct {
-		name string
-		c    *py.Class
-	}{
-		{"Region", &_regionClass},
-		{"RegionSet", &_region_setClass},
-		{"View", &_viewClass},
-		{"Window", &_windowClass},
-		{"Edit", &_editClass},
-		{"Settings", &_settingsClass},
-		{"WindowCommandGlue", &_windowCommandGlueClass},
-		{"TextCommandGlue", &_textCommandGlueClass},
-		{"ApplicationCommandGlue", &_applicationCommandGlueClass},
-		{"OnQueryContextGlue", &_onQueryContextGlueClass},
-		{"ViewEventGlue", &_viewEventGlueClass},
-	}
-	constants := []struct {
-		name     string
-		constant int
-	}{
-		{"OP_EQUAL", int(util.OpEqual)},
-		{"OP_NOT_EQUAL", int(util.OpNotEqual)},
-		{"OP_REGEX_MATCH", int(util.OpRegexMatch)},
-		{"OP_NOT_REGEX_MATCH", int(util.OpNotRegexMatch)},
-		{"OP_REGEX_CONTAINS", int(util.OpRegexContains)},
-		{"OP_NOT_REGEX_CONTAINS", int(util.OpNotRegexContains)},
-		{"INHIBIT_WORD_COMPLETIONS", 0},
-		{"INHIBIT_EXPLICIT_COMPLETIONS", 0},
-		{"LITERAL", int(backend.IGNORECASE)},
-		{"IGNORECASE", int(backend.LITERAL)},
-		{"CLASS_WORD_START", int(backend.CLASS_WORD_START)},
-		{"CLASS_WORD_END", int(backend.CLASS_WORD_END)},
-		{"CLASS_PUNCTUATION_START", int(backend.CLASS_PUNCTUATION_START)},
-		{"CLASS_PUNCTUATION_END", int(backend.CLASS_PUNCTUATION_END)},
-		{"CLASS_SUB_WORD_START", int(backend.CLASS_SUB_WORD_START)},
-		{"CLASS_SUB_WORD_END", int(backend.CLASS_SUB_WORD_END)},
-		{"CLASS_LINE_START", int(backend.CLASS_LINE_START)},
-		{"CLASS_LINE_END", int(backend.CLASS_LINE_END)},
-		{"CLASS_EMPTY_LINE", int(backend.CLASS_EMPTY_LINE)},
-		{"CLASS_MIDDLE_WORD", int(backend.CLASS_MIDDLE_WORD)},
-		{"CLASS_WORD_START_WITH_PUNCTUATION", int(backend.CLASS_WORD_START_WITH_PUNCTUATION)},
-		{"CLASS_WORD_END_WITH_PUNCTUATION", int(backend.CLASS_WORD_END_WITH_PUNCTUATION)},
-		{"CLASS_OPENING_PARENTHESIS", int(backend.CLASS_OPENING_PARENTHESIS)},
-		{"CLASS_CLOSING_PARENTHESIS", int(backend.CLASS_CLOSING_PARENTHESIS)},
-		{"DRAW_EMPTY", int(render.DRAW_EMPTY)},
-		{"HIDE_ON_MINIMAP", int(render.HIDE_ON_MINIMAP)},
-		{"DRAW_EMPTY_AS_OVERWRITE", int(render.DRAW_EMPTY_AS_OVERWRITE)},
-		{"DRAW_NO_FILL", int(render.DRAW_NO_FILL)},
-		{"DRAW_NO_OUTLINE", int(render.DRAW_NO_OUTLINE)},
-		{"DRAW_SOLID_UNDERLINE", int(render.DRAW_SOLID_UNDERLINE)},
-		{"DRAW_STIPPLED_UNDERLINE", int(render.DRAW_STIPPLED_UNDERLINE)},
-		{"DRAW_SQUIGGLY_UNDERLINE", int(render.DRAW_SQUIGGLY_UNDERLINE)},
-		{"PERSISTENT", int(render.PERSISTENT)},
-		{"HIDDEN", int(render.HIDDEN)},
-	}
-
-	for _, cl := range classes {
-		c, err := cl.c.Create()
-		if err != nil {
-			panic(err)
-		}
-		if err := m.AddObject(cl.name, c); err != nil {
-			panic(err)
-		}
-	}
-	for _, c := range constants {
-		if err := m.AddIntConstant(c.name, c.constant); err != nil {
-			panic(err)
-		}
-	}
+	{Name: "error_message", Func: sublime_ErrorMessage},
+	{Name: "message_dialog", Func: sublime_MessageDialog},
+	{Name: "ok_cancel_dialog", Func: sublime_OkCancelDialog},
+	{Name: "status_message", Func: sublime_StatusMessage},
 }
